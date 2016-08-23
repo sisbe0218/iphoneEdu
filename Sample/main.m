655834c2600bdb76043cde8fe0711f05669af521
user <user@users-imac-3.local>
2016-08-23 15:35:37 +0900

    Initial Commit

---
+++ Sample/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sample
+//
+//  Created by user on 2016. 8. 23..
+//  Copyright © 2016년 LDCC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
